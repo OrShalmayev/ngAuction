9356aeeb8a6912173f44aba13ef6d2a9a20c83d7
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2022-12-26 15:21:26 +0200

    onPush

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import {ProductService} from "../shared/product.service";
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent  {
